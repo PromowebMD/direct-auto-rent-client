0e066edc1be03356d7a0ac81d05bdb9ef62e6c6d
DumbravaM <42375617+dumbrava2001@users.noreply.github.com>
2024-02-27 15:29:12 +0200

    Replace contact bar icons

--- src/components/ContactBar.tsx
+++ src/components/ContactBar.tsx
@@ -1,17 +1,18 @@
-import { MapPin, Phone } from "react-feather";
+import locationIcon from "../assets/location_icon.svg";
+import phoneIcon from "../assets/phone_icon.svg";
 
 export const ContactBar = () => {
   return (
-    <div className="hidden h-fit w-full max-w-[1000px] justify-end text-sm font-normal text-white bg-transparent font-noToSans lg:flex">
+    <div className="hidden h-fit w-full justify-end bg-transparent text-sm font-normal text-white max-w-[1000px] font-noToSans lg:flex">
       <div className="flex justify-end">
         <div className="flex gap-6 py-1">
           <div className="flex items-center gap-4">
-            <MapPin className="w-[20px] h-[20px]" />
+            <img src={locationIcon} alt="" className="w-[18px] h-[18px]" />
             <p>Ștefan cel Mare 123</p>
             <p>Dacia 80/3</p>
           </div>
           <div className="flex items-center gap-4">
-            <Phone className="w-[20px] h-[20px]" />
+            <img src={phoneIcon} alt="" className="w-[16px] h-[16px]" />
             <p>+373 (79) 167 777</p>
             <p>+373 (79) 167 777</p>
           </div>
