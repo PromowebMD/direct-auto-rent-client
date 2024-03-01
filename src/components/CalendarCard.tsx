7611f0fd9cf39a96cd6d27916978d2c4cc5df4c4
DumbravaM <42375617+dumbrava2001@users.noreply.github.com>
2024-03-01 15:56:26 +0200

    Refactor calendar card text

--- src/components/CalendarCard.tsx
+++ src/components/CalendarCard.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import { Check } from "react-feather";
-import { CALENDAR_CANCEL_RESERVATION } from "../utils/appText.ts";
 import { CalendarSelector } from "./CalendarSelector.tsx";
 
 export const CalendarCard: React.FC = () => {
   return (
     <div className="grid w-full place-items-center">
-      <div className="hidden w-full items-center text-lg font-normal text-white font-noToSans md:flex">
-        <Check />
-        {CALENDAR_CANCEL_RESERVATION}
-      </div>
       <div className="flex w-full justify-center">
         <CalendarSelector />
       </div>
