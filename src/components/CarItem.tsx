7ba5561ffb275d99f8791694d6d9ba87d24b78d6
DumbravaM <42375617+dumbrava2001@users.noreply.github.com>
2024-03-07 15:56:05 +0200

    Refactor car item

--- src/components/CarItem.tsx
+++ src/components/CarItem.tsx
@@ -34,21 +34,28 @@ export const CarItem: React.FC<CarItemProps> = ({ carItem, className }) => {
     <div
       className={`flex w-full items-start gap-2 font-oswald lg:w-fit lg:items-center lg:gap-6 ${className}`}
     >
-      <h2 className="text-2xl lg:hidden">{`${carItem.title} ${carItem.year}`.toUpperCase()}</h2>
-      <span className="max-w-[370px]">
+      <h2 className="text-2xl lg:hidden">
+        {`${carItem.title} ${carItem.year}`.toUpperCase()}
+      </h2>
+      <span className="max-w-[220px] lg:max-w-[370px]">
         <img src={carItem.primaryImage} alt="" />
       </span>
       <div className="flex w-full flex-col gap-2 lg:w-fit">
-        <h2 className="hidden text-xl lg:block">{`${carItem.title} ${carItem.year}`.toUpperCase()}</h2>
-        <div className="grid grid-cols-3 gap-2 font-normal lg:grid-cols-2">
+        <h2 className="hidden text-xl lg:block">
+          {`${carItem.title} ${carItem.year}`.toUpperCase()}
+        </h2>
+        <div className="flex grid-cols-2 flex-wrap gap-2 font-normal lg:grid">
           {carDescriptionList.map((item) => {
             return (
-              <div key={item.label} className="flex w-32 lg:w-fit items-center gap-1">
+              <div
+                key={item.label}
+                className="flex items-center gap-1 lg:w-fit"
+              >
                 <span>
                   <img src={item.icon} alt="" className="w-[18px] h-[30px]" />
                 </span>
                 <p
-                  className={`text-sm lg:text-base ${item.icon === distanceLimit && "text-primary"}`}
+                  className={`text-sm lg:text-base ${item.icon === distanceLimit && "text-primary mr-8 lg:mr-0"}`}
                 >
                   {item.label}
                 </p>
